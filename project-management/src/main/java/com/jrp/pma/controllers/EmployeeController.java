32f85a8ae21907bceb47531ea6b868d3429f2bfe
Pranaya Shrestha <pshrest2@go.olemiss.edu>
2020-10-22 11:46:22 -0500

    Created a MVC framework in Spring that adds employees and project information in database

---
+++ project-management/src/main/java/com/jrp/pma/controllers/EmployeeController.java
@@ -0,0 +1,44 @@
+package com.jrp.pma.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jrp.pma.dao.EmployeeRepository;
+import com.jrp.pma.entities.Employee;
+
+@Controller
+@RequestMapping("/employee")
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeRepository empRepo;
+	
+	@GetMapping("/new")
+	public String displayProjectForm(Model model) {
+		
+		Employee anEmployee = new Employee();
+		
+		model.addAttribute("employee", anEmployee);
+		return "new-employee.html";
+	}
+	
+	@PostMapping("/save")
+	public String createEmployeeForm(Employee employee, Model model) {
+		//this should handel saving to the database...
+		empRepo.save(employee);
+		
+		//user a redirect to prevent duplicate submissions
+		return "redirect:/employee/new";
+	}
+	
+}
+
+
+
+
+
+
---
+++ project-management/src/main/java/com/jrp/pma/controllers/ProjectController.java
@@ -0,0 +1,44 @@
+package com.jrp.pma.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.jrp.pma.dao.EmployeeRepository;
+import com.jrp.pma.dao.ProjectRepository;
+import com.jrp.pma.entities.Employee;
+import com.jrp.pma.entities.Project;
+
+@Controller
+@RequestMapping("/projects")
+public class ProjectController {
+
+	@Autowired //we don't have to manually create instance for ProjectRepository, spring framework will do it
+	ProjectRepository proRepo;
+	
+	@GetMapping("/new")
+	public String displayProjectForm(Model model) {
+		
+		Project aProject = new Project();
+		
+		model.addAttribute("project", aProject);
+		return "new-project.html";
+	}
+	
+	@PostMapping("/save")
+	public String createProjectForm(Employee employee, Project project, Model model) {
+		//this should handel saving to the database...
+		proRepo.save(project);		
+		//user a redirect to prevent duplicate submissions
+		return "redirect:/projects/new";
+	}	
+}
+
+
+
+
+
+
